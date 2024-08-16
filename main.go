2b9268bb750e4ee457959b0146d250e9bb437d92
vivek <infovivek2020@gmail.com>
2024-08-16 17:18:15 +0530

    added main file in remote

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{-2,1,-3,4,-1,2,1,-5,4}
+	temp := 0
+	for i:=len(nums)-1;i<len(nums);i--{
+temp +=nums[i]
+if temp> {
+	
+}
+	}
+}
+Problem: Maximum Subarray Sum
+Description:
+Given an integer array nums, find the contiguous subarray having the largest sum and return its sum.
+ 
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+The subarray [4,-1,2,1] has the largest sum = 6.
